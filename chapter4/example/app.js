038683fc28253ae7a23fa6145ce268abcd6836df
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-05-31 09:20:22 +0500

    Исправление ошибки

--- chapter4/example/app.js
+++ chapter4/example/app.js
@@ -1,9 +1,9 @@
 var main = function () {
     "use strict";
     $(".comment-input button").on("click", function (event) {
-        var $new_comment = $("<p>"),
-        comment_text = $(".comment-input input").val();
-        $new_comment.text(comment_text);
+    var $new_comment;
+    if ($(".comment-input input").val() !== "") {
+        $new_comment = $("<p>").text($(".comment-input input").val());
         $(".comments").append($new_comment);
     });
 };
