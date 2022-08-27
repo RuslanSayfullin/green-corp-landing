933933785acead30851d8d6a777aa601b2dcebab
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-08-27 21:49:55 +0500

    Создание класса CanvasBackground

--- js/canvas.js
+++ js/canvas.js
@@ -1,4 +1,6 @@
 const COLORS = ["255,108,80", "5,117,18", "29,39,57", "67,189,81"];
+const BUBBLE_DENSITY = 100;
+
 function generateDecimalBetween(left, right) {
   return (Math.random() * (left - right) + right).toFixed(2);
 }
@@ -46,3 +48,48 @@ bubbles.push(new Bubble(canvas));
 bubbles.push(new Bubble(canvas));
 
 console.log(bubbles);
+
+class CanvasBackground {
+  constructor(id) {
+    this.canvas = document.getElementById(id);
+    this.ctx = this.canvas.getContext("2d");
+    this.dpr = window.devicePixelRatio;
+
+  }
+
+  start() {
+    this.canvasSize();
+    this.generateBubbles();
+    this.animate();
+  }
+  canvasSize() {
+   this.canvas.width = this.canvas.offsetWidth * this.dpr;
+   this.canvas.height = this.canvas.offsetHeight * this.dpr;
+
+   this.ctx.scale(this.dpr, this.dpr);
+   }
+   generateBubbles() {
+       this.bubblesList = [];
+       for (let i = 0; i < BUBBLE_DENSITY; i++) {
+       this.bubblesList.push(new Bubble(this.canvas))
+        }
+   }
+   animate() {
+   this.ctx.clearRect(0, 0, this.canvas.clientWidth,
+   this.canvas.clientHeight);
+
+    this.bubblesList.forEach((bubble) => {
+       bubble.move();
+       this.ctx.translate(bubble.translateX, bubble.translateY);
+       this.ctx.beginPath();
+       this.ctx.arc(0, 0, bubble.size, 0, 2 * Math.PI);
+       this.ctx.fillStyle = "rgba(" + bubble.color + "," + bubble.alpha + ")";
+       this.ctx.fill();
+       this.ctx.setTransform(this.dpr, 0, 0, this.dpr, 0, 0);
+       requestAnimationFrame(this.animate.bind(this));
+       });
+    }
+}
+
+const canvas = new CanvasBackground("orb-canvas");
+canvas.start();
